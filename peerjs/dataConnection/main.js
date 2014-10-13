e26c02dc18b2bfe12d9ca8f53f9748ce503519e4
Andres Chort <andres.chort@gmail.com>
2014-10-13 11:54:03 -0300

    dataConnection

--- peerjs/dataConnection/main.js
+++ peerjs/dataConnection/main.js
@@ -1,4 +1,6 @@
-$(document).on('ready', function () { $('#disconnect').hide(); });
+$(document).on('ready', function () {
+    $('#disconnect').hide();
+});
 
 var peer = new Peer({host: 'localhost', port: 9000, path: 'myapp'});
 var connection;
@@ -13,30 +15,46 @@ peer.on('connection', function(conn) {
     conn.on('open', function () { connected(conn);});
     
     conn.on('data', function(data){
-        var chatLog = $('#chatLog');
-        chatLog.val(chatLog.val() + "\n Other: " + data);
+        $('#chatLog').append("\nEl: " + data);
     });
+
+    conn.on('close', function() { disconnected(); });
 });
 
 function connect() {
     var targetId = $('#targetId').val();
     connection = peer.connect(targetId);
-    connection.on('open', function(){
-        $('#targetIdLabel').text('Conectado con');
-        $('#targetId').attr('disabled', true);
-        $('#connect').hide();
-        $('#disconnect').show();
+    connection.on('open', function(){ connected(connection); });
+    connection.on('close', function() { disconnected();});
+    connection.on('data', function(data) {
+        $('#chatLog').append("\nEl: " + data);
     });
 }
 
 function send() {
-    connection.send($('#input').val());
+    var data = $('#input').val();
+    $('#chatLog').append('\nYo: ' + data);
+    connection.send(data);
+    $('#input').val('').focus();
 }
 
 function disconnect() {
     connection.close();
+    disconnected();
+}
+
+function disconnected() {
+    $('#targetIdLabel').text('Id destino');
+    $('#targetId').attr('disabled', false);
+    $('#connect').show();
+    $('#disconnect').hide();
+    $('#chatLog').append('Desconectado de ' + conn.peer);
 }
 
 function connected(conn) {
-    
+    $('#targetIdLabel').text('Conectado con');
+    $('#targetId').val(conn.peer).attr('disabled', true);
+    $('#connect').hide();
+    $('#disconnect').show();
+    $('#chatLog').append('\nConectado con ' + conn.peer);
 }
